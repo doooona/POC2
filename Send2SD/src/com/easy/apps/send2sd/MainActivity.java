b297af80c2c9b8b8d4728faf44b61d1c020ef9ef
doooona.lee <doooona.lee@gmail.com>
2014-07-01 18:05:11 +0800

    [POC2-SD] Add logic

--- Send2SD/src/com/easy/apps/send2sd/MainActivity.java
+++ Send2SD/src/com/easy/apps/send2sd/MainActivity.java
@@ -1,9 +1,30 @@
 package com.easy.apps.send2sd;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
 import info.hoang8f.widget.FButton;
 
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build.VERSION;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.storage.StorageManager;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
@@ -12,6 +33,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
 	
 	private FButton twitterBtn;
 	private FButton openBtn;
+	private Uri photoURI;
+	private StorageManager mStorageManager;
+	private Method mMethodGetPaths;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -58,5 +82,130 @@ public class MainActivity extends Activity implements View.OnClickListener{
 	private void showToast(String msg)
 	{
 		Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+	}
+	
+	private void saveImage()
+	{
+		if (photoURI != null) {
+			try {
+				String fileName = photoURI.toString().substring(
+						2 + photoURI.toString().lastIndexOf("/"),
+						photoURI.toString().length());
+				
+				int sysVersion = Integer.parseInt(VERSION.SDK);
+				
+				String sdPath;
+				
+				if(sysVersion > 11){
+					
+					String[] paths = this.getVolumePaths();
+					sdPath = paths[1];
+					
+				}else{
+					sdPath = Environment.getExternalStorageDirectory()
+							.getAbsolutePath();
+				}
+				
+				Log.d("@@@", "sdPath:" + sdPath);
+				
+				String finalFilePath = sdPath + File.separator
+						+ "Download Facebook photos";
+				File sdFolder = new File(finalFilePath);
+				if (!sdFolder.exists())
+					sdFolder.mkdir();
+
+				copy(new File(photoURI.getPath()), new File(finalFilePath,
+						fileName));
+
+				String[] pathAry = { finalFilePath + File.separator + fileName };
+
+				MediaScannerConnection.scanFile(this, pathAry, null, null);
+
+				Toast.makeText(this, this.getString(R.string.save_suc),
+						Toast.LENGTH_LONG).show();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void copy(File inFile, File outFile){
+		OutputStream out = null;
+		try {
+			out = new FileOutputStream(outFile);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			copyFile(new FileInputStream(inFile),out);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void copyFile(InputStream in, OutputStream out) throws IOException {
+	    byte[] buffer = new byte[1024];	
+	    int read;
+	    while((read = in.read(buffer)) != -1){
+	      out.write(buffer, 0, read);
+	    }
+	}
+	
+	private void openFolder()
+	{
+		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+		Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath()
+				+ "/Download Facebook photos/");
+		intent.setDataAndType(uri, "image/*");
+		startActivity(Intent.createChooser(intent, "Open folder"));
+	}
+	
+	public String[] getVolumePaths() {
+		String[] paths = null;
+		try {
+			mStorageManager = (StorageManager)this
+					.getSystemService(Activity.STORAGE_SERVICE);
+			
+			mMethodGetPaths = mStorageManager.getClass()
+					.getMethod("getVolumePaths");
+			
+			paths = (String[]) mMethodGetPaths.invoke(mStorageManager);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+			return paths;
+	}
+	
+	private String getCallingAppName()
+	{
+		String appname = null;
+		ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+		List<ActivityManager.RecentTaskInfo> recentTasks = am.getRecentTasks(10000,ActivityManager.RECENT_WITH_EXCLUDED);
+		
+		String packageName = recentTasks.get(1).baseIntent.getComponent().getClassName();
+		PackageInfo packageInfo;
+		try {
+			packageInfo = this.getPackageManager().getPackageInfo(packageName, 0);
+		
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return appname;
 	}
 }
