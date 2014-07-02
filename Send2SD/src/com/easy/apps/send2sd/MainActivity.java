a4d6e72a1d087b89edbb0bbc95e65db789b82b96
doooona.lee@gmail.com <doooona.lee@gmail.com>
2014-07-03 00:20:54 +0800

    [POC2-SD] Add languages

--- Send2SD/src/com/easy/apps/send2sd/MainActivity.java
+++ Send2SD/src/com/easy/apps/send2sd/MainActivity.java
@@ -39,6 +39,75 @@ public class MainActivity extends Activity implements View.OnClickListener{
 	private Method mMethodGetPaths;
 	private static final String SAVE_FLODER_NAME = "SaveToSD";
 	
+	private final String[][] MIME_MapTable={ 
+            {".3gp",    "video/3gpp"}, 
+            {".apk",    "application/vnd.android.package-archive"}, 
+            {".asf",    "video/x-ms-asf"}, 
+            {".avi",    "video/x-msvideo"}, 
+            {".bin",    "application/octet-stream"}, 
+            {".bmp",    "image/bmp"}, 
+            {".c",  "text/plain"}, 
+            {".class",  "application/octet-stream"}, 
+            {".conf",   "text/plain"}, 
+            {".cpp",    "text/plain"}, 
+            {".doc",    "application/msword"}, 
+            {".docx",   "application/vnd.openxmlformats-officedocument.wordprocessingml.document"}, 
+            {".xls",    "application/vnd.ms-excel"},  
+            {".xlsx",   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"}, 
+            {".exe",    "application/octet-stream"}, 
+            {".gif",    "image/gif"}, 
+            {".gtar",   "application/x-gtar"}, 
+            {".gz", "application/x-gzip"}, 
+            {".h",  "text/plain"}, 
+            {".htm",    "text/html"}, 
+            {".html",   "text/html"}, 
+            {".jar",    "application/java-archive"}, 
+            {".java",   "text/plain"}, 
+            {".jpeg",   "image/jpeg"}, 
+            {".jpg",    "image/jpeg"}, 
+            {".js", "application/x-javascript"}, 
+            {".log",    "text/plain"}, 
+            {".m3u",    "audio/x-mpegurl"}, 
+            {".m4a",    "audio/mp4a-latm"}, 
+            {".m4b",    "audio/mp4a-latm"}, 
+            {".m4p",    "audio/mp4a-latm"}, 
+            {".m4u",    "video/vnd.mpegurl"}, 
+            {".m4v",    "video/x-m4v"},  
+            {".mov",    "video/quicktime"}, 
+            {".mp2",    "audio/x-mpeg"}, 
+            {".mp3",    "audio/x-mpeg"}, 
+            {".mp4",    "video/mp4"}, 
+            {".mpc",    "application/vnd.mpohun.certificate"},        
+            {".mpe",    "video/mpeg"},   
+            {".mpeg",   "video/mpeg"},   
+            {".mpg",    "video/mpeg"},   
+            {".mpg4",   "video/mp4"},    
+            {".mpga",   "audio/mpeg"}, 
+            {".msg",    "application/vnd.ms-outlook"}, 
+            {".ogg",    "audio/ogg"}, 
+            {".pdf",    "application/pdf"}, 
+            {".png",    "image/png"}, 
+            {".pps",    "application/vnd.ms-powerpoint"}, 
+            {".ppt",    "application/vnd.ms-powerpoint"}, 
+            {".pptx",   "application/vnd.openxmlformats-officedocument.presentationml.presentation"}, 
+            {".prop",   "text/plain"}, 
+            {".rc", "text/plain"}, 
+            {".rmvb",   "audio/x-pn-realaudio"}, 
+            {".rtf",    "application/rtf"}, 
+            {".sh", "text/plain"}, 
+            {".tar",    "application/x-tar"},    
+            {".tgz",    "application/x-compressed"},  
+            {".txt",    "text/plain"}, 
+            {".wav",    "audio/x-wav"}, 
+            {".wma",    "audio/x-ms-wma"}, 
+            {".wmv",    "audio/x-ms-wmv"}, 
+            {".wps",    "application/vnd.ms-works"}, 
+            {".xml",    "text/plain"}, 
+            {".z",  "application/x-compress"}, 
+            {".zip",    "application/x-zip-compressed"}, 
+            {"",        "*/*"}   
+        }; 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -91,6 +160,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
 			 
 		 	case R.id.f_twitter_button2:
 		 	
+		 		openFolder();
+		 		
 		 	break;
 		 }
 	}
@@ -289,8 +360,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
 	{
 		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
 		Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath()
-				+ "/Download Facebook photos/");
-		intent.setDataAndType(uri, "image/*");
+				+ File.separator +SAVE_FLODER_NAME + File.separator);
+		intent.setDataAndType(uri, "text/csv");
 		startActivity(Intent.createChooser(intent, "Open folder"));
 	}
 	
@@ -334,4 +405,24 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		
 		return appname;
 	}
+	
+	private String getMIMEType(File file) { 
+	     
+	    String type="*/*"; 
+	    String fName = file.getName(); 
+	 
+	    int dotIndex = fName.lastIndexOf("."); 
+	    if(dotIndex < 0){ 
+	        return type; 
+	    } 
+	    
+	    String end=fName.substring(dotIndex,fName.length()).toLowerCase(); 
+	    if(end=="")return type; 
+	    
+	    for(int i=0;i<MIME_MapTable.length;i++){ 
+	        if(end.equals(MIME_MapTable[i][0])) 
+	            type = MIME_MapTable[i][1]; 
+	    }        
+	    return type; 
+	} 
 }
